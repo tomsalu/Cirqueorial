b714b89e27af1a41f12e3d9c0e2927eaf2542709
tomsalu <tas9@aber.ac.uk>
2013-02-22 15:47:09 +0000

    Revert "Revert "Revert "Revert "FIXED - Parse User Table works. Old UserView deleted""""
    This reverts commit ffc7ca9e054d4fd6a3e3f76a294d9fb1496b9045.

--- Cirqueorial/Cirqueorial/AppDelegate.m
+++ Cirqueorial/Cirqueorial/AppDelegate.m
@@ -9,8 +9,6 @@
 #import "AppDelegate.h"
 #import "FMDatabase.h"
 #import <Parse/Parse.h>
-#import "UserViewController.h"
-
 
 
 @implementation AppDelegate
@@ -29,10 +27,7 @@ NSString *const FBSessionStateChangedNotification =
 
     [Parse setApplicationId:@"YN1ow77nwfbLryiAu7XsqXpqxigWjTlTmQL025ax"
                   clientKey:@"PgGSZ7HyMqXo9YAy8xeq0tKg7mpWhZWGaUogBber"];
-    
-    UserViewController *controller = [[UserViewController alloc] init];
-    
-    self.window.rootViewController = controller;
+        
     [self.window makeKeyAndVisible];
     
     
